2bdaf05119d4a851eabbb67313a75d9bfe6d1726
Sam Noh <samnoh21@outlook.com>
2019-08-05 18:57:56 +1200

    mod - clean up

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,8 @@ import { MatchReader } from './match';
 import { Summary } from './summary';
 
 const matchReader = MatchReader.fromCsv('data/football.csv');
-const summary1 = Summary.winsAnalysisWithConsoleReport('Man United');
-const summary2 = Summary.averageGoalsAnalysisWithConsoleReport('Liverpool');
-
 matchReader.load();
-summary1.buildAndPrintReport(matchReader.matches);
-summary2.buildAndPrintReport(matchReader.matches);
+
+Summary.winsAnalysisWithConsoleReport('Man United').buildAndPrintReport(matchReader.matches);
+
+Summary.averageGoalsAnalysisWithConsoleReport('Liverpool').buildAndPrintReport(matchReader.matches);
