3a1832caaa471d5e643d148719e77436f9d7b108
Sam Noh <samnoh21@outlook.com>
2019-08-05 12:03:01 +1200

    add - enum

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,23 @@
 import fs from 'fs';
 
-const matches = fs.readFileSync('football.csv', {
-    encoding: 'utf-8'
-});
+enum MatchResult {
+    HomeWin = 'H',
+    AwayWin = 'A',
+    Draw = 'D'
+}
 
-console.log(matches);
+const matches = fs
+    .readFileSync('data/football.csv', {
+        encoding: 'utf-8'
+    })
+    .split('\n')
+    .map((row: string): string[] => row.split(','));
+
+let manUnitedWins = 0;
+
+for (let match of matches) {
+    if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) manUnitedWins++;
+    else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) manUnitedWins++;
+}
+
+console.log(manUnitedWins);
